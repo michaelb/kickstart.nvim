65505175dc666925737efaec840191944be0464d
Michael Bleuez <michael.bleuez2@gmail.com>
2025-07-15 19:07:04 +0200

    smaller neotree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -15,14 +15,14 @@ return {
   },
   opts = {
     close_if_last_window = true,
-    window = { mappings = { ["<space>"] = "none" } },
+    window = { mappings = { ['<space>'] = 'none' } },
     filesystem = {
       follow_current_file = {
-        enabled = true,            -- This will find and focus the file in the active buffer every time
-                       -- the current file is changed while the tree is open.
-        leave_dirs_open = false,   -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        -- the current file is changed while the tree is open.
       },
       window = {
+        width = 28,
         mappings = {
           ['esc'] = 'close_window',
         },
